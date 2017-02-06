4a5d9b064801f1dcf2794f2d56d90cd0eacce911
Alex Studnička <AStudnicka@gmail.com>
2017-02-06 22:10:29 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "HashedPassword",
     dependencies: [
-		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0)
+		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0, minor: 6)
     ]
 )
